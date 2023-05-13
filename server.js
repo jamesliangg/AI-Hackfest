ae13729eb9142513f9c26267b1f36f3b780ad705
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-05-12 21:25:19 -0400

    Simplify

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+const app = express();
+import bodyParser from "body-parser";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res, next) {
+    res.send('Try calling the API endpoint');
+});
+
+app.post('/api/endpoint', async function (req, res) {
+    const data = req.body;
+    // const action = req.body.action;
+    console.log('Received data:', data);
+
+    let response = "No action received.";
+    res.send(response);
+});
+
+app.listen(3000, function() {
+    console.log('Server listening at http://127.0.0.1:3000/api/endpoint');
+});
