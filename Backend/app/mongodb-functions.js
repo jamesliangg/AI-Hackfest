311826153a9d849dca123e52e32990d40cc16bf2
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-05-13 19:18:34 -0400

    Rest of MongoDB functions

--- Backend/app/mongodb-functions.js
+++ Backend/app/mongodb-functions.js
@@ -4,11 +4,11 @@ import {MongoClient} from "mongodb";
 let uri = process.env.MONGO_CONNECT_STRING;
 const client = new MongoClient(uri);
 
-export async function mongoQuery(queryKey, queryValue) {
-    let queryResult = "Error in query"
+export async function mongoQueryOne(queryKey, queryValue, mongoDatabase, mongoCollection) {
+    let queryResult = "Error in query";
     try {
-        const database = client.db('apollo');
-        const collection = database.collection('ingredients');
+        const database = client.db(mongoDatabase);
+        const collection = database.collection(mongoCollection);
 
         const query = { [queryKey]: queryValue };
         queryResult = await collection.findOne(query);
@@ -21,16 +21,14 @@ export async function mongoQuery(queryKey, queryValue) {
     }
 }
 
-export async function mongoInsert() {
-    let insertResult = "Error in insert"
+export async function mongoInsertOne(input, mongoDatabase, mongoCollection) {
+    let insertResult = "Error in insert";
     try {
-        const database = client.db('apollo');
-        const collection = database.collection('ingredients');
+        const database = client.db(mongoDatabase);
+        const collection = database.collection(mongoCollection);
 
-        const insert = {
-            ingredient: 'bubble tea',
-            health: true
-        };
+        let insert = {};
+        Object.assign(insert, input);
 
         insertResult = await collection.insertOne(insert);
 
@@ -39,5 +37,27 @@ export async function mongoInsert() {
         // Ensures that the client will close when you finish/error
         await client.close();
         return insertResult;
+    }
+}
+
+export async function mongoUpdateOne(queryKey, queryValue, input, mongoDatabase, mongoCollection) {
+    let updateResult = "Error in update";
+    try {
+        const database = client.db(mongoDatabase);
+        const collection = database.collection(mongoCollection);
+
+        let insert = {};
+        Object.assign(insert, input);
+
+        updateResult = await collection.updateOne(
+            {[queryKey]: queryValue},
+            {$set: insert}
+        );
+
+        console.log(updateResult);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+        return updateResult;
     }
 }
--- Backend/app/server.js
+++ Backend/app/server.js
@@ -3,7 +3,7 @@ const app = express();
 import bodyParser from "body-parser";
 import {cohereDetectLanguage} from "./cohere-functions.js";
 import {deepTranslate} from "./deepl-functions.js";
-import {mongoInsert, mongoQuery} from "./mongodb-functions.js";
+import {mongoInsertOne, mongoUpdateOne, mongoQueryOne} from "./mongodb-functions.js";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,7 +14,7 @@ app.get('/', function(req, res, next) {
 
 app.post('/api/endpoint', async function (req, res) {
     const data = req.body;
-    // const action = req.body.action;
+    const action = req.body.action;
     console.log('Received data: ', data);
     let response = "No action received.";
 
@@ -29,14 +29,28 @@ app.post('/api/endpoint', async function (req, res) {
     // let deeplResponse =  await deepTranslate(sampleText, "fr");
 
     console.log("MongoDB key: " + process.env.MONGO_CONNECT_STRING);
-    let mongoResponse = await mongoQuery("ingredient", "bubble tea");
-    // let mongoResponse = await mongoInsert();
+    const insert = {
+        ingredient: 'bubble tea',
+        health: true,
+        filler: false,
+        taste: true
+    };
+    const update = {
+        health: false,
+        filler: true,
+        taste: false
+    };
+    const queryKey = "ingredient";
+    const queryValue = "bubble tea";
+    // let mongoResponse = await mongoQuery(queryKey, queryValue, process.env.MONGO_DATABASE, process.env.MONGO_COLLECTION);
+    // let mongoResponse = await mongoInsertOne(insert, process.env.MONGO_DATABASE, process.env.MONGO_COLLECTION);
+    // let mongoResponse = await mongoUpdateOne(queryKey, queryValue, update, process.env.MONGO_DATABASE, process.env.MONGO_COLLECTION);
 
     response = JSON.stringify({
-        "action": req.body.action,
+        "action": action,
         // "cohere": cohereResponse,
         // "deepl": deeplResponse,
-        "mongo": mongoResponse
+        // "mongo": mongoResponse
     });
 
     res.send(response);
