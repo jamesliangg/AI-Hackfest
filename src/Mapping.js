b31945dadcf50be7032c05068cdfd48df1dd8759
Danny <d28yang@uwaterloo.ca>
2023-05-14 01:19:41 -0400

    all map function is in Mapping.js

--- src/Mapping.js
+++ src/Mapping.js
@@ -1,21 +1,32 @@
 
 import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from "@material-ui/core";
-import Mapping from './Mapping';
 
 //import Map from './components/Map/Map';
 import Map from "./components/Map/Map";
 import Header from "./components/Header/Header";
 
-function App() {
+function Mapping() {
   const [autocomplete, setAutocomplete] = useState(null);
 
 
   return (
     <div>
-      <Mapping/>
+      <CssBaseline />
+      {/* <Header/> */}
+      <Grid container spacing={3} style={{ width: "100%" }}>
+
+        <Grid item xs={12} md={4}>
+          <div>List</div>
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <Map />
+        </Grid>
+        
+      </Grid>
     </div>
   );
 }
 
-export default App;
+export default Mapping;
